9e300f7eb5d1b9926a6a0b3c0040a4c851b656e8
rdgupta280 <rdgupta280@gmail.com>
2023-04-20 18:47:17 +0530

    solution1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import {Fragment} from 'react'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+        <Reactbenifit/>
+        <Button/>
       </header>
     </div>
+  );
+}
+
+
+function Reactbenifit(){
+  return (
+    <div class="App-header">
+    <h1>Benifits of using react ...</h1>
+    <ol>
+        <li>Benifits of usiing react</li>
+        <li>Component-based architecture</li>
+        <li>Virtual DOM for efficient Updates</li>
+        <li>Rich ecosystem and community</li>
+        <li>Cross-platform development</li>
+        <li>Strong community support</li>
+    </ol>
+   </div>
+
+  );
+}
+
+
+function Button(){
+  return (
+    <button>Get Started...</button>
   );
 }
 
