4d7e4245dcd4c598dcff0a7cc3fcc7cb026884ad
rdgupta280 <rdgupta280@gmail.com>
2023-05-02 19:26:29 +0530

    Creating Simple Greeting Website

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// 17th Video
+// Challenge-3
+// React JS Mini-Project #1: Creating Simple Greeting Website using React JS
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import React from "react";
+import ReactDOM from "react-dom";
+import './index.css';
+
+let curDate = new Date(2023, 5, 1, 2);
+curDate = curDate.getHours();
+let greeting = "";
+
+const cssStyle = {};
+
+if (curDate >= 1 && curDate < 12) {
+  greeting = "Good Morning";
+  cssStyle.color = "green";
+} else if (curDate >= 12 && curDate < 19) {
+  greeting = "Good Afternoon";
+  cssStyle.color = "Orange";
+} else {
+  greeting = "Good Night";
+  cssStyle.color = "black";
+}
+
+ReactDOM.render(
+  <>
+    <div>
+      <h1>
+        Hello Sir, <span style={cssStyle}> {greeting} </span>
+      </h1>
+    </div>
+  </>,
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+
+
